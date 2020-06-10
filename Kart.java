a0024b332b50af5a6ced989a67503acc5bc31bec
apreda1 <thespeed_@hotmail.com>
2020-06-09 20:28:53 -0600

    Create Kart.java

---
+++ Kart.java
@@ -0,0 +1,31 @@
+
+package array6hafizaexercise;
+
+public class Kart {
+    private char variable;
+    private boolean guess = false;
+
+    public Kart(char variable) {
+        this.variable = variable;
+    }
+
+    public char getVariable() {
+        return variable;
+    }
+
+    public void setVariable(char variable) {
+        this.variable = variable;
+    }
+
+    public boolean isGuess() {
+        return guess;
+    }
+
+    public void setGuess(boolean guess) {
+        this.guess = guess;
+    }
+    
+    
+    
+    
+}
